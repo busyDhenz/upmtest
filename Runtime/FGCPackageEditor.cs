ad55e12adb974f6dcd4f196725dbe5f4a7e69a45
Dhenz Zerrudo <dhenz.zerrudo@synergy88digital.com>
2019-07-15 11:40:00 +0800

    test runtime folder

---
+++ Runtime/FGCPackageEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class FGCPackageEditor : EditorWindow
+{  
+    private static Vector2 WindowSize = new Vector2(400, 600);
+    
+    [MenuItem("Synergy88/FGC/Essential")]
+    public static void Init()
+    {
+        var window =  GetWindow<FGCPackageEditor>(true);
+        window.minSize = WindowSize;
+        window.maxSize = WindowSize;
+        window.Show();
+        window.InitializeWindow();
+
+    }
+
+    private void InitializeWindow()
+    {
+        var file = MonoScript.FromScriptableObject(this);
+        var dir = AssetDatabase.GetAssetPath(file);
+        var path = Application.dataPath;
+        Debug.LogError(path+dir);
+    }
+
+}
